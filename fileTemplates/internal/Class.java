6a27de94c3f119fde7e4c510dbe3ee3e24c631ce
niu <niu@DESKTOP-HCAMON2>
2020-11-20 17:58:23 +0800

    IU-2020.2.3 <niu@DESKTOP-HCAMON2 Create markdown.xml, IntelliLang.xml, print.xml, project.default.xml, diff.xml, equalsHashCodeTemplates.xml, Default.xml, applicationLibraries.xml, Retta.icls, Monokai Sublime Text 3.icls, find.xml, File Header.java, keymap.xml, Zenburnesque.icls, _@user_Material Oceanic.icls, _@user_Darcula.icls, mavenVersion.xml, Tomorrow Night.icls, Default.xml, Atom.icls, One Dark.icls, _@user_Material Darker.icls, Emacs.icls, Eclipse copy.xml, Peacock.icls, LAZY.icls, editor.xml, debugger.xml, updates.xml, baseRefactoring.xml, ide.general.xml, other.xml, git.xml, _@user_Default.icls, Mac Classic.icls, shared-indexes.xml, ui.lnf.xml, gradle.settings.xml, filetypes.xml, projectView.xml, plugin_ui.xml, colors.scheme.xml, notifications.xml, databaseDrivers.xml, templates.xml, Sunburst.icls, myTemp.xml, laf.xml, parameter.hints.xml, Class.java, jdk.table.xml, path.macros.xml, Eiffel.icls, web-browsers.xml, wsl.distributions.xml, _@user_Atom One Dark.icls, proxy.settings.xml, vcs.xml, databaseSettings.xml, grazie_global.xml, toStringTemplates.xml, IDLE.icls, Wombat.icls, usageView.xml

---
+++ fileTemplates/internal/Class.java
@@ -0,0 +1,9 @@
+#if (${PACKAGE_NAME} && ${PACKAGE_NAME} != "")package ${PACKAGE_NAME};#end
+/**
+ * @author 牛顺顺
+ * @description ${description}
+ * @date ${DATE}
+ * @copyright 广州瑞云网络科技有限公司
+ */
+public class ${NAME} {
+}
